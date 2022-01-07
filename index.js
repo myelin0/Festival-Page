31c8dabed76104e61b14e9807b2cdd782086fc76
Emi <emi.veisllari@gmail.com>
2022-01-07 22:23:45 +0100

    Improve feature image quality

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ const container = document.querySelector('.feature-container');
 for (let k = 0; k < cards.length; k += 1) {
   const featureProject = document.createElement('div');
   featureProject.className = 'featWrap';
+  console.log(featureProject)
   let featureContent = '';
   featureContent = `
 <div class="feature">
